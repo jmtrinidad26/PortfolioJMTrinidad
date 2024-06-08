ae42a0993f2dda76eacfefb7af34e1dafde3873e
jmtrinidad26 <jmtrinidad22@gmail.com>
2024-06-08 09:31:27 +0800

    test

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const darkModeBtn = document.getElementById('darkModeBtn');
-    
     darkModeBtn.addEventListener('click', function() {
+        //toggle sun and moon dark mode
         darkModeBtn.classList.toggle('clicked-image');
-        document.body.classList.toggle('dark-mode');
+        //toggle dark mode class in css
+        document.body.classList.toggle('dark-mode');	
+    });	
 
-    });
-
-   
-});
+});x    
