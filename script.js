26893ef81dce30150f876bfd464a2701222d1d53
jmtrinidad26 <jmtrinidad22@gmail.com>
2024-06-08 09:36:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const darkModeBtn = document.getElementById('darkModeBtn');
     darkModeBtn.addEventListener('click', function() {
         //toggle sun and moon dark mode
-        darkModeBtn.classList.toggle('clicked-image');
+        darkModeBtn.classList.toggle('clic  ked-image');
         //toggle dark mode class in css
         document.body.classList.toggle('dark-mode');	
     });	
