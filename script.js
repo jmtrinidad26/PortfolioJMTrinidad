6e8556cf13404324c9b8bd2ce0eefde09344ecf0
jmtrinidad26 <jmtrinidad22@gmail.com>
2024-06-08 10:13:28 +0800

    new

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const darkModeBtn = document.getElementById('darkModeBtn');
     darkModeBtn.addEventListener('click', function() {
         //toggle sun and moon dark mode
-        darkModeBtn.classList.toggle('clic  ked-image');
+        darkModeBtn.classList.toggle('clicked-image');
         //toggle dark mode class in css
         document.body.classList.toggle('dark-mode');	
     });	
 
-});x    
+});
+function toggleReadMore() {
+    var moreText = document.querySelector('.read-more');
+    var showMoreBtn = document.querySelector('.show-more');
+
+    if (moreText.style.display === "none" || moreText.style.display === "") {
+        moreText.style.display = "block";
+        showMoreBtn.textContent = "Read Less";
+    } else {
+        moreText.style.display = "none";
+        showMoreBtn.textContent = "Read More";
+    }
+}    
